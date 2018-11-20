2d6fe729b3f5a2b7183b6f8a54d49abc073f10c3
deda9 <Bassem.Qoulta@gmail.com>
2018-11-20 14:42:30 +0100

    Add Demo

---
+++ UIRangeSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIRangeSlider
+//
+//  Created by Bassem Qoulta on 11/20/18.
+//  Copyright © 2018 Bassem Qoulta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slider: UIRangeSlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
